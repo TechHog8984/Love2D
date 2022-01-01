3d72b678e3578601a48804dd3e99a71beef7de59
TechHog <77560013+TechHog8984@users.noreply.github.com>
2022-01-01 01:00:38 +0000

    Update ObjectHandler.lua

--- ObjectHandler.lua
+++ ObjectHandler.lua
@@ -1,3 +1,62 @@
+local old = table
+local table = {}
+
+function table.count(Table)
+    local count = 0
+    for I,V in next, Table do
+        if V ~= nil then
+            count = count + 1
+        end
+    end
+    return count
+end
+
+function table.insert(Table, Value)
+    local length = table.count(Table)
+
+    Table[length + 1] = Value
+end
+
+function table.find(Table, Value)
+    for I, V in next, Table do
+        if V and Value and V == Value then
+            return I, V
+        end
+    end
+end
+function table.findbyindex(Table, Index)
+	for I, V in next, Table do
+		if I and V and Index and I == Index then
+			return I, V
+		end
+	end
+end
+
+function table.findlower(Table, Value)
+    for I, V in next, Table do
+        if V and Value and tostring(V):lower() == tostring(Value):lower() then
+            return I, V
+        end
+    end
+end
+function table.findlowerbyindex(Table, Index)
+	for I, V in next, Table do
+        if I and Index and tostring(I):lower() == tostring(Index):lower() then
+            return I, V
+        end
+    end
+end
+
+function table.remove(Table, Value)
+    local index = (table.find(Table, Value))
+    if index ~= nil then
+        Table[index] = nil
+    end
+end
+function table.removebyindex(Table, Index)
+    Table[Index] = nil
+end
+
 local Class = {
 	Objects = {},
 }
@@ -30,19 +89,62 @@ Class.BaseObjectMetatable = {
 		return self.Name
 	end,
 }
+Class.DefaultProperties = {
+	Rectangle = {
+		Visible = {
+			type = 'boolean',
+			default = true,
+		},
+		Opacity = {
+			type = 'number',
+			default = 1,
+		},
+
+		Fill = {
+			type = 'boolean',
+			default = true,
+		},
+		Outline = {
+			type = 'boolean',
+			default = false,
+		},
+
+		X = {
+			type = 'number',
+			default = 0,
+		},
+		Y = {
+			type = 'number',
+			default = 0,
+		},
+
+		Width = {
+			type = 'number',
+			default = 0,
+		},
+		Height = {
+			type = 'number',
+			default = 0,
+		},
+	}
+}
 
 do --helper / info functions
+	function Class.Color(R, G, B)
+		return {R or 1, G or 1, B or 1, 1}
+	end
+	function Class.ColorRGB(R, G, B)
+		return {(R / 255) or 1, (G / 255) or 1, (B / 255) or 1, 1}
+	end
+
 	function Class:IsColliding(o1, o2)
 		local CollidingX = (o1.X + o1.Width) > o2.X and (o1.X) < (o2.X + o2.Width)
 		local CollidingY = (o1.Y + o1.Height) > o2.Y and (o1.Y) < (o2.Y + o2.Height)
 
 		return (CollidingX and CollidingY), CollidingX, CollidingY
 	end
-	function Class:Color(R, G, B, O)
-		return {R or 1, G or 1, B or 1, O or 1}
-	end
-	function Class:ColorRGB(R, G, B, O)
-		return {(R / 255) or 1, (G / 255) or 1, (B / 255) or 1, O or 1}
+	function Class:DoesObjectExist(Object)
+		return Object and rawget(Object, '__exists') == true
 	end
 
 	function Class:UpdateWindow()
@@ -57,35 +159,42 @@ do --helper / info functions
 	end
 
 	function Class:GetExpectedProperties(Name)
-		if Name == 'Rectangle' then
-			return [[
-				<boolean> Fill 		=    true
-				<boolean> Outline 	=   false
+		if self.DefaultProperties[Name] then
+			local Str = ''
 
-				<number> X =   0
-				<number> Y =   0
+			for Property, Info in next, self.DefaultProperties[Name] do
+				Str = Str .. '<' .. Info.type .. '> ' .. Property .. ' = ' .. tostring(Info.default) .. '\n'
+			end
 
-				<number> Width 	=   0
-				<number> Height = 	0
-			]]
+			return Str
+		else
+			error('Failed to find properties for ' .. tostring(Name), 2)
 		end
 	end
 end
 
 do --main object creation functions
 	function Class:CreateObject(Type, Gargs, Info, Metatable)
+		local Self = self
 		local Object = {
 			__gtype = Type,
 			__gargs = Gargs,
+			__exists = true,
 		}
 
 		for I, V in next, Info do
 			Object[I] = V
 		end
 
+		function Object:Destroy()
+			self.__exists = false
+			table.remove(Self.Objects, Object)
+			self = nil
+		end
+
 		setmetatable(Object, TableAddIndex(Class.BaseObjectMetatable, Metatable or {}))
 
-		table.insert(self.Objects, Object)
+		table.insert(Self.Objects, Object)
 
 		return Object
 	end
@@ -95,6 +204,8 @@ do --main object creation functions
 			local Extras = {
 				Name = info.Name or '[OH object]#' .. tostring(#self.Objects + 1),
 				Color = info.Color or {1, 1, 1, 1},
+				Opacity = 1,
+				Visible = true,
 				_x = info.X or 0,
 				_y = info.Y or 0,
 				_width = info.Width or 0,
@@ -129,17 +240,13 @@ do --main object creation functions
 				end,
 				__newindex = function(Self, Index, Value)
 					if Index == 'X' then
-						rawset(Self, '_x', Value)
-						return rawset(Self.__gargs, 2, Value)
+						return rawset(Self.__gargs, 2, Value), rawset(Self, '_x', Value)
 					elseif Index == 'Y' then
-						rawset(Self, '_y', Value)
-						return rawset(Self.__gargs, 3, Value)
+						return rawset(Self.__gargs, 3, Value), rawset(Self, '_y', Value)
 					elseif Index == 'Width' then
-						rawset(Self, '_width', Value)
-						return rawset(Self.__gargs, 4, Value)
+						return rawset(Self.__gargs, 4, Value), rawset(Self, '_width', Value)
 					elseif Index == 'Height' then
-						rawset(Self, '_height', Value)
-						return rawset(Self.__gargs, 5, Value)
+						return rawset(Self.__gargs, 5, Value), rawset(Self, '_height', Value)
 					end
 
 					return rawset(Self, Index, Value)
@@ -157,8 +264,11 @@ function Class:Draw()
 	if self.Graphics then
 		for I, Object in next, self.Objects do
 			if Object and type(Object) == 'table' and rawget(Object, '__gtype') and rawget(Object, '__gargs') then
-				self.Graphics.setColor(unpack(Object.Color))
-				self.Graphics[rawget(Object, '__gtype')](unpack(rawget(Object, '__gargs')))
+				if Object.Visible == true then
+					local r, g, b = unpack(Object.Color)
+					self.Graphics.setColor(r, g, b, Object.Opacity)
+					self.Graphics[rawget(Object, '__gtype')](unpack(rawget(Object, '__gargs')))
+				end
 			else
 				error('Failed to draw object, ensure object exists, is a table, has __gtype and has __gargs. (failed checks relating to the object existing itself and its properties)', 2)
 			end
